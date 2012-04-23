6ffd329b4a193dc6ad8e2fffe655e74fe66ee0e1
tzlloch@gmail.com <tzlloch@gmail.com@0441bf06-048d-d659-6b34-d1af66c0f4fa>
2012-04-23 03:12:36 +0000

    1.1 stable

---
+++ branches/1.1/test/test_rand.cpp
@@ -0,0 +1,33 @@
+#include <time.h>
+#include <stdio.h>
+#include <assert.h>
+#include <ulib/rand_tpl.h>
+
+int main()
+{
+	uint64_t u, v, w;
+	uint64_t seed = time(0);
+
+	RAND_NR_INIT(u, v, w, seed);
+
+	for (int i = 0; i < 100; i++)
+		printf("rand number = %llx\n", (unsigned long long)RAND_NR_NEXT(u, v, w));
+
+	uint64_t h = 0;
+
+	printf("rand int mix1 = %llx\n", (unsigned long long)RAND_INT_MIX64(h));
+	printf("rand int mix2 = %llx\n", (unsigned long long)RAND_INT2_MIX64(h));
+
+	for (int i = 0; i < 1000; ++i) {
+		uint64_t s = h;
+		RAND_INT3_MIX64(h);
+		uint64_t t = h;
+		RAND_INT3_MIX64_INV(t);
+		printf ("mix3: s = %016lx, t = %016lx\n", s, t);
+		assert(s == t);
+	}
+
+	printf("passed\n");
+
+	return 0;
+}
